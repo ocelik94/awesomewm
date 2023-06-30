fda7dc8fa91c84315038be7801d4b59499efb34b
Okan Celik <okan.celik@projectoc.de>
2023-06-30 17:03:52 +0000

    Update file init.lua

--- bar/modules/battery/init.lua
+++ bar/modules/battery/init.lua
@@ -1,17 +1,40 @@
 local wibox = require("wibox")
 local awful = require("awful")
 
-function update_mouse_battery_icon(icon)
-    local iconName = awful.util.pread(
-    "upower -i /org/freedesktop/UPower/devices/mouse_0003o046Do101Ax0006 " ..
-    "| grep icon-name | grep -oP \"(?<=').*(?=')\"")
-    icon:set_image("/usr/share/icons/gnome/scalable/status/" .. string.gsub(iconName, "\n", "") .. ".svg")
-end
+local mybattery = awful.widget.watch(
+    { awful.util.shell, "-c", "upower -i /org/freedesktop/UPower/devices/battery_BAT | sed -n '/present/,/icon-name/p'" },
+    30,
+    function(widget, stdout)
+        local bat_now = {
+            present      = "N/A",
+            state        = "N/A",
+            warninglevel = "N/A",
+            energy       = "N/A",
+            energyfull   = "N/A",
+            energyrate   = "N/A",
+            voltage      = "N/A",
+            percentage   = "N/A",
+            capacity     = "N/A",
+            icon         = "N/A"
+        }
 
-mouse_battery_icon = wibox.widget.imagebox()
-mouse_battery_timer = timer({ timeout = 60 })
-mouse_battery_timer:connect_signal("timeout",  function() update_mouse_battery_icon(mouse_battery_icon) end)
-mouse_battery_timer:start()
-mouse_battery_timer:emit_signal("timeout")
+        for k, v in string.gmatch(stdout, '([%a]+[%a|-]+):%s*([%a|%d]+[,|%a|%d]-)') do
+            if     k == "present"       then bat_now.present      = v
+            elseif k == "state"         then bat_now.state        = v
+            elseif k == "warning-level" then bat_now.warninglevel = v
+            elseif k == "energy"        then bat_now.energy       = string.gsub(v, ",", ".") -- Wh
+            elseif k == "energy-full"   then bat_now.energyfull   = string.gsub(v, ",", ".") -- Wh
+            elseif k == "energy-rate"   then bat_now.energyrate   = string.gsub(v, ",", ".") -- W
+            elseif k == "voltage"       then bat_now.voltage      = string.gsub(v, ",", ".") -- V
+            elseif k == "percentage"    then bat_now.percentage   = tonumber(v)              -- %
+            elseif k == "capacity"      then bat_now.capacity     = string.gsub(v, ",", ".") -- %
+            elseif k == "icon-name"     then bat_now.icon         = v
+            end
+        end
 
-return mouse_battery_icon
+        -- customize here
+        widget:set_text("Bat: " .. bat_now.percentage .. " " .. bat_now.state)
+    end
+)
+
+return mybattery
