a7e8f13634a66847b8ca587db7fc1de0daabbc16
Okan Celik <okan@celik.tech>
2023-07-05 19:59:42 +0200

    fix: autostarts

--- autostart.lua
+++ autostart.lua
@@ -11,5 +11,17 @@ awful.spawn.easy_async_with_shell(
     function() awful.spawn("nm-applet --indicator") end
 )
 
--- awful.spawn("xrandr --output HDMI-0 --primary --mode 1280x960 --rate 144")
-awful.spawn("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 144")
+awful.spawn.easy_async_with_shell(
+    'ps x | grep "picom" | grep -v grep | awk "{print $1}" | xargs kill',
+    function() awful.spawn("picom") end
+)
+
+awful.spawn.easy_async_with_shell(
+    'ps x | grep "vmware-user-suid-wrapper" | grep -v grep | awk "{print $1}" | xargs kill',
+    function() awful.spawn("vmware-user-suid-wrapper") end
+)
+
+awful.spawn.easy_async_with_shell(
+    'ps x | grep "imwheel" | grep -v grep | awk "{print $1}" | xargs kill',
+    function() awful.spawn("imwheel") end
+)
