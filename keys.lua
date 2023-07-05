8f38413a54661e347e64d8a3a61f3a6353dc1813
Okan Celik <okan.celik@projectoc.de>
2023-07-05 12:36:10 +0000

    Update file keys.lua

--- keys.lua
+++ keys.lua
@@ -2,7 +2,7 @@ local awful = require("awful.init")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
 local modkey = "Mod4"
-local terminal = "kitty"
+local terminal = "wezterm"
 
 -- General Awesome keys
 awful.keyboard.append_global_keybindings({
