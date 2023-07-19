5104dc05006fe7454c7e52285aa1d69e889f18f3
Okan Celik <okan.celik@projectoc.de>
2023-07-19 18:30:31 +0000

    fix: remove autostart entries

--- autostart.lua
+++ autostart.lua
@@ -10,23 +10,3 @@ awful.spawn.easy_async_with_shell(
     'ps x | grep "nm-applet" | grep -v grep | awk "{print $1}" | xargs kill',
     function() awful.spawn("nm-applet --indicator") end
 )
-
-awful.spawn.easy_async_with_shell(
-    'ps x | grep "polkit-kde-authentication-agent-1" | grep -v grep | awk "{print $1}" | xargs kill',
-    function() awful.spawn("/usr/lib/polkit-kde-authentication-agent-1") end
-)
-
-awful.spawn.easy_async_with_shell(
-    'ps x | grep "picom" | grep -v grep | awk "{print $1}" | xargs kill',
-    function() awful.spawn("picom") end
-)
-
-awful.spawn.easy_async_with_shell(
-    'ps x | grep "vmware-user-suid-wrapper" | grep -v grep | awk "{print $1}" | xargs kill',
-    function() awful.spawn("vmware-user-suid-wrapper") end
-)
-
-awful.spawn.easy_async_with_shell(
-    'ps x | grep "imwheel" | grep -v grep | awk "{print $1}" | xargs kill',
-    function() awful.spawn("imwheel") end
-)
